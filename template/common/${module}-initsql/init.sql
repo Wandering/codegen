ac1a7938375bd11b32d2df128136b9d05f79267c
pli <pli@thinkjoy.cn>
2014-09-24 16:04:48 +0800

    初始化SQL

---
+++ template/common/${module}-initsql/init.sql
@@ -0,0 +1,34 @@
+-- model
+<#list tables as table>
+<#assign className = table.classNameBo>
+<#assign classNameLower = className?uncap_first>
+INSERT INTO `model`(`id`,`name`,`tblName`)
+VALUES(${table_index+1},'${table.remarks}','${table.sqlName}');
+</#list>
+
+-- resource
+<#assign resourceId=1/>
+<#assign resourceGridId=1/>
+<#list tables as table>
+
+<#assign className = table.classNameBo>
+<#assign classNameFirstLower = table.classNameFirstLower>
+<#assign classNameAllLower = table.classNameBo?lower_case>
+
+INSERT INTO `resource`(`id`,`url`,`orderNum`,`parentId`,`number`,`longNumber`,`name`,`creator`,`createDate`,`lastModifier`,`lastModDate`,`description`,`modelId`)
+VALUES(${resourceId},'/admin/${classNameAllLower}',1,null,'a','a_1','${table.remarks}',0,0,0,0,null,null);
+
+  <#assign className = table.classNameBo>
+  <#assign classNameFirstLower = table.classNameFirstLower>
+  <#assign idJavaType = table.idColumn.javaType>
+  <#macro mapperEl value>${r"#{"}${value}}</#macro>
+  <#macro namespace>${basepackage}.${persistence}</#macro>
+
+  -- resource_grid
+  <#list table.columns as column>
+      INSERT INTO `resource_grid` (`id`,`resId`,`displayName`,`colId`,`orderNum`,`width`,`editoptions`,`edittype`,`unformat`)
+      VALUES(${resourceGridId},${resourceId},'${column.remarks}','${column.sqlName}',${column_index},200,'{}',null,null);
+      <#assign resourceGridId=resourceGridId+1/>
+  </#list>
+<#assign resourceId=resourceId+1/>
+</#list>
