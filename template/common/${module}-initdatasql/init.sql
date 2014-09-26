308a04cce0f44aaa016aff0a1a698a7d0082da12
pli <pli@thinkjoy.cn>
2014-09-26 12:04:57 +0800

    支持CRUD

--- template/common/${module}-initdatasql/init.sql
+++ template/common/${module}-initdatasql/init.sql
@@ -2,7 +2,7 @@
 <#list tables as table>
 <#assign className = table.classNameBo>
 <#assign classNameLower = className?uncap_first>
-INSERT INTO `model`(`id`,`name`,`tblName`)
+INSERT INTO `ehr_model`(`id`,`name`,`tblName`)
 VALUES(${table_index+1},'${table.remarks}','${table.sqlName}');
 </#list>
 
@@ -15,7 +15,7 @@ VALUES(${table_index+1},'${table.remarks}','${table.sqlName}');
 <#assign classNameFirstLower = table.classNameFirstLower>
 <#assign classNameAllLower = table.classNameBo?lower_case>
 
-INSERT INTO `resource`(`id`,`url`,`orderNum`,`parentId`,`number`,`longNumber`,`name`,`creator`,`createDate`,`lastModifier`,`lastModDate`,`description`,`modelId`)
+INSERT INTO `ehr_resource`(`id`,`url`,`orderNum`,`parentId`,`number`,`longNumber`,`name`,`creator`,`createDate`,`lastModifier`,`lastModDate`,`description`,`modelId`)
 VALUES(${resourceId},'/admin/${classNameAllLower}',1,null,'a','a_1','${table.remarks}',0,0,0,0,null,null);
 
   <#assign className = table.classNameBo>
@@ -26,8 +26,8 @@ VALUES(${resourceId},'/admin/${classNameAllLower}',1,null,'a','a_1','${table.rem
 
   -- resource_grid
   <#list table.columns as column>
-      INSERT INTO `resource_grid` (`id`,`resId`,`displayName`,`colId`,`orderNum`,`width`,`editoptions`,`edittype`,`unformat`,`description`)
-      VALUES(${resourceGridId},${resourceId},'${column.remarks}','${column.sqlName}',${column_index},200,'{}',null,null,'');
+      INSERT INTO `ehr_resource_grid` (`id`,`resId`,`displayName`,`colId`,`orderNum`,`width`,`editoptions`,`edittype`,`unformat`,`description`,`moduleName`)
+      VALUES(${resourceGridId},${resourceId},'${column.remarks}','${column.sqlName}',${column_index},200,'{}',null,null,'','${classNameAllLower}');
       <#assign resourceGridId=resourceGridId+1/>
   </#list>
 <#assign resourceId=resourceId+1/>
