a9f1a9f9fd470b37ea63774706aac6ec8bf10d64
wdong <872239537@qq.com>
2015-12-07 20:31:44 +0800

    添加oracle支持分表

--- templateOracle/mybatis/${module}-facade/I${classNameBo}Facade.java
+++ templateOracle/mybatis/${module}-facade/I${classNameBo}Facade.java
@@ -1,6 +1,7 @@
 <#assign className = table.classNameBo>
 <#assign classNameLower = className?uncap_first>
 <#assign classNameAllLower = table.classNameBo?lower_case>
+
 /*
  * Copyright (c) 2013-2014, thinkjoy Inc. All Rights Reserved.
  *
