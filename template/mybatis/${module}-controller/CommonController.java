fb269e013e1488f6e38845d8da82d755c486c28c
wdong <872239537@qq.com>
2016-01-05 22:43:48 +0800

    修改版本号

--- template/mybatis/${module}-controller/CommonController.java
+++ template/mybatis/${module}-controller/CommonController.java
@@ -17,10 +17,13 @@ import cn.thinkjoy.common.managerui.controller.AbstractCommonController;
 import cn.thinkjoy.common.service.IBaseService;
 import ${basepackage}.common.ServiceMaps;
 
+
+import com.mysql.jdbc.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
