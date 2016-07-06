f003cde2d6e6900bc2304bb3bc1149f1b8b63163
xule <xule@thinkjoy.cn>
2016-07-06 14:05:38 +0800

    常用字段formatter添加

--- common/webapp/assets/js/biz/module/formatter.js
+++ common/webapp/assets/js/biz/module/formatter.js
@@ -4,20 +4,20 @@
  */
 /**
  * 状态格式化
-  * @param cellvalue
+ * @param cellvalue
  * @param options
  * @param rowObject
  * @returns {string}
  */
-function formatExpertState(cellvalue, options, rowObject){
-        if("1" == cellvalue){
-            return "显示";
-        } else if("2" == cellvalue){
-            return "隐藏";
-        } else { //3
-            return "冻结";
-        }
+function formatExpertState(cellvalue, options, rowObject) {
+    if ("1" == cellvalue) {
+        return "显示";
+    } else if ("2" == cellvalue) {
+        return "隐藏";
+    } else { //3
+        return "冻结";
     }
+}
 /**
  * 公司名称格式化
  * @param cellvalue
@@ -25,84 +25,137 @@ function formatExpertState(cellvalue, options, rowObject){
  * @param rowObject
  * @returns {string}
  */
-    function formatExpertType(cellvalue, options, rowObject){
-        if("2000" == cellvalue){
-            return "习悦";
-        } else if("2001" == cellvalue){
-            return "全通";
+function formatExpertType(cellvalue, options, rowObject) {
+    if ("2000" == cellvalue) {
+        return "习悦";
+    } else if ("2001" == cellvalue) {
+        return "全通";
+    }
+}
+/**
+ * 隐藏 undefined
+ * @param cellvalue
+ * @returns {*}
+ * @author yhwang
+ */
+function undefinedHide(cellvalue) {
+    if (cellvalue == undefined) {
+        return "";
+    } else {
+        return cellvalue;
+    }
+}
+/**
+ * 输入毫秒 输出 xx时xx分xx秒
+ * @param cellvalue
+ * @returns {string}
+ * @author yhwang
+ */
+function formatSeconds(cellvalue) {
+    var theTime = parseInt(cellvalue);// 秒
+    var theTime1 = 0;// 分
+    var theTime2 = 0;// 小时
+    if (theTime > 60) {
+        theTime1 = parseInt(theTime / 60);
+        theTime = parseInt(theTime % 60);
+        if (theTime1 > 60) {
+            theTime2 = parseInt(theTime1 / 60);
+            theTime1 = parseInt(theTime1 % 60);
         }
     }
-    /**
-     * 隐藏 undefined
-     * @param cellvalue
-     * @returns {*}
-     * @author yhwang
-     */
-    function undefinedHide(cellvalue){
-        if(cellvalue==undefined){
-            return "";
-        }else{
-            return cellvalue;
-        }
+    var result = "" + parseInt(theTime) + "秒";
+    if (theTime1 > 0) {
+        result = "" + parseInt(theTime1) + "分" + result;
     }
-    /**
-     * 输入毫秒 输出 xx时xx分xx秒
-     * @param cellvalue
-     * @returns {string}
-     * @author yhwang
-     */
-    function formatSeconds(cellvalue) {
-        var theTime = parseInt(cellvalue);// 秒
-        var theTime1 = 0;// 分
-        var theTime2 = 0;// 小时
-        if(theTime > 60) {
-            theTime1 = parseInt(theTime/60);
-            theTime = parseInt(theTime%60);
-            if(theTime1 > 60) {
-                theTime2 = parseInt(theTime1/60);
-                theTime1 = parseInt(theTime1%60);
-            }
-        }
-        var result = ""+parseInt(theTime)+"秒";
-        if(theTime1 > 0) {
-            result = ""+parseInt(theTime1)+"分"+result;
-        }
-        if(theTime2 > 0) {
-            result = ""+parseInt(theTime2)+"小时"+result;
-        }
-        return result;
+    if (theTime2 > 0) {
+        result = "" + parseInt(theTime2) + "小时" + result;
     }
-    /**
-     * 日期 long 显示为 yyyy-MM-dd hh:mm:ss
-     * @param cellvalue
-     * @returns {string}
-     */
-    function formatdate(cellvalue){
-        if(cellvalue == null || cellvalue=='' ||cellvalue ==0){
-            return "";
-        }
-        var date= new Date(cellvalue);
-        var month = (date.getMonth()+1);
-        var dates = date.getDate();
-        var hour = date.getHours();
-        var minutes = date.getMinutes();
-        var seconds = date.getSeconds();
-        if(month < 10){
-            month = "0"+month;
-        }
-        if(dates < 10){
-            dates = "0"+dates;
-        }
-        if(hour < 10){
-            hour = "0"+hour;
-        }
-        if(minutes < 10){
-            minutes = "0"+minutes;
-        }
-        if(seconds < 10){
-            seconds = "0"+seconds;
-        }
+    return result;
+}
+/**
+ * 日期 long 显示为 yyyy-MM-dd hh:mm:ss
+ * @param cellvalue
+ * @returns {string}
+ */
+function formatdate(cellvalue) {
+    if (cellvalue == null || cellvalue == '' || cellvalue == 0) {
+        return "";
+    }
+    var date = new Date(cellvalue);
+    var month = (date.getMonth() + 1);
+    var dates = date.getDate();
+    var hour = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    if (month < 10) {
+        month = "0" + month;
+    }
+    if (dates < 10) {
+        dates = "0" + dates;
+    }
+    if (hour < 10) {
+        hour = "0" + hour;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    return date.getFullYear() + "-" + month + "-" + dates + " " + hour + ":" + minutes + ":" + seconds;
+}
 
-
-        return date.getFullYear()+"-"+month+"-"+dates+" "+hour+":"+minutes+":"+seconds;
+//日期 long 显示为 yyyy-MM-dd
+function formatshortdate(cellvalue) {
+    if (cellvalue == null || cellvalue == '' || cellvalue == 0) {
+        return "";
     }
+    var date = new Date(cellvalue);
+    var month = (date.getMonth() + 1);
+    var dates = date.getDate();
+    if (month < 10) {
+        month = "0" + month;
+    }
+    if (dates < 10) {
+        dates = "0" + dates;
+    }
+    return date.getFullYear() + "-" + month + "-" + dates;
+}
+
+function formatState(cellvalue) {
+    if ("10" == cellvalue) {
+        return "正常";
+    } else if ("20" == cellvalue) {
+        return "停用";
+    } else { //30
+        return "逻辑删除";
+    }
+}
+
+function formatIsParent(cellvalue) {
+    if ("true" == cellvalue) {
+        return "是";
+    } else {
+        return "否"
+    }
+}
+
+function formatHide(cellvalue) {
+    if ("1" == cellvalue) {
+        return "是";
+    } else if ("0" == cellvalue) {
+        return "否";
+    } else { //30
+        return "逻辑删除";
+    }
+}
+
+function formatSex(cellvalue) {
+    if ("1" == cellvalue) {
+        return "男";
+    } else if ("2" == cellvalue) {
+        return "女";
+    } else { //30
+        return "未知";
+    }
+}
